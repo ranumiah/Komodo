aca0dcfd18809119342ede49569b7331dae4232f
Ranu <ranu.miah@outlook.com>
2019-01-30 12:39:49 +0000

    Install ngx-bootstrap and use on nav bar

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { AlertifyService } from './_services/alertify.service';
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      BsDropdownModule.forRoot(),
       AppRoutingModule
    ],
    providers: [
