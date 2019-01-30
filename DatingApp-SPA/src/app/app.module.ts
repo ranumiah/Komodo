944755700f31af287cae2a2721949adcabd0f1a9
Ranu <ranu.miah@outlook.com>
2019-01-30 14:32:55 +0000

    Delete default angular roots

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 
@@ -25,8 +24,7 @@ import { AlertifyService } from './_services/alertify.service';
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot(),
-      AppRoutingModule
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
