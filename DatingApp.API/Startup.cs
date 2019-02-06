c8fb17f0acd367497277b35301324aeec5a5ffad
Ranu <O@O.net>
2019-02-06 16:17:41 +0000

    comment out the database seeder

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -86,7 +86,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            seeder.SeedUsers();
+            //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
