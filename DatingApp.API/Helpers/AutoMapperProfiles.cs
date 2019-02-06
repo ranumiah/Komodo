cb0b3f8214c9ae13bd4f017eaf18679b58ff5459
Ranu <O@O.net>
2019-02-06 18:04:04 +0000

    Add api end point for persisting the member profile update

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Helpers
                     map.MapFrom(m => m.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
