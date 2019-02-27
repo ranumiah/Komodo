0a7947ef2020a00c34b4804280460507437f12ac
Ranu <O@O.net>
2019-02-27 16:51:13 +0000

    Using Action Filters

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -59,6 +59,7 @@ namespace DatingApp.API
 
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
