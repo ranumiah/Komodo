dca7dd13e910c23125b195b41cec14f318bbd2d9
Ranu <O@O.net>
2019-03-06 11:47:42 +0000

    Adding the Send Like functionality in the API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -82,5 +82,11 @@ namespace DatingApp.API.Data
             return await _context.Photos.Where(u => u.UserId == userId)
                 .FirstOrDefaultAsync(p => p.IsMain);
         }
+
+        public async Task<Like> GetLike(int userId, int recipientId)
+        {
+            return await _context.Likes.FirstOrDefaultAsync(u =>
+                u.LikerId == userId && u.LikeeId == recipientId);
+        }
     }
 }
